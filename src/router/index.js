c89ff9ea329526bbbf33e3a2de4d1f7f57aef22a
LorenitaTL <lore7096@gmail.com>
2021-06-06 10:32:25 -0500

    CRUD complete

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Crear from '../components/Crear.vue';
+import Editar from '../components/Editar.vue';
+import Listar from '../components/Listar.vue';
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/crear',
+    name: 'Crear',
+    component: Crear
+  },
+  {
+    path: '/editar/:id',
+    name: 'Editar',
+    component: Editar
+  },
+  {
+    path: '/listar',
+    name: 'Listar',
+    component: Listar
   },
   {
     path: '/about',
